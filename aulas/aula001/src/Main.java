a56fa23669de89f84b66f7f54ac67090b457dc20
joaovitor-amaro <joaovitoramaro71@gmail.com>
2024-11-01 09:55:46 -0300

    Aula 1 - Primeiro comando em Java

---
+++ aulas/aula001/src/Main.java
@@ -0,0 +1,11 @@
+public class Main { //Classe principal
+    //Primeiro metodo que o JVM chama ao executar o codigo.
+    public static void main(String[] args) {
+        //Comando de saida de texto.
+        //quebra linha
+        System.out.println("Hello, World");
+        //Comando de saida de texto que nao quebra linha
+        System.out.print("Ola, vitor ");
+        System.out.println("Amaro");
+    }
+}
