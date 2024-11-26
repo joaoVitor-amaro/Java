8297264d3730cda6c85efa3f5cca5946b166db20
joaovitor-amaro <joaovitoramaro71@gmail.com>
2024-11-26 20:04:07 -0300

    Aula 3 - Operadores relacionais

---
+++ aulas/aula003/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Operadores relacionais
+        //Retornam valores boolean
+        boolean isDezMaiorQvinte = 10 >= 20; //Operador maior ou igual que
+        System.out.println("Maior que vinte: " + isDezMaiorQvinte);
+        boolean isDezMenorQquinte = 10 <= 20; //Operador Menor ou igual que
+        System.out.println("Menor que: " + isDezMenorQquinte);     
+        boolean isDezIgualVinte = 10 == 20;//OPerador de igualdade
+        System.out.println("Igual: " + isDezIgualVinte);
+        boolean isDezDiferenteVinte = 10 != 20; //OPerador diferente
+        System.out.println("Diferente: " + isDezDiferenteVinte);
+        // < operador menor que
+        // > Operador maior que 
+    }
+}
