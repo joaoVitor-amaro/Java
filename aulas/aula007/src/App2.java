7b018ca8edde43afa6f89e9c51f43c7f88c1855b
joaovitor-amaro <joaovitoramaro71@gmail.com>
2024-12-03 10:48:13 -0300

    Aula 7 - Arrays

--- aulas/aula007/src/App2.java
+++ aulas/aula007/src/App2.java
@@ -1,8 +1,8 @@
 public class App2 {
     public static void main(String[] args) {
         String[] nomes = new String[3];
-        nomes[0] = "Rayssa";
-        nomes[1] = "Joao Vitor";
+        nomes[0] = "Vitor";
+        nomes[1] = "Joao";
         nomes[2] = "Pedro";
         //Percorre os valores do array por meio dos indices
         for(int i = 0; i < nomes.length; i++) {
