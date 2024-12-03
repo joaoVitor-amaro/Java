f46d5f80feb23c7bde60f8dc59bd219b75d6ba57
joaovitor-amaro <joaovitoramaro71@gmail.com>
2024-12-02 22:26:01 -0300

    Ex 3 - Numeros pares de um for entre 0 e 40

---
+++ Exercicios/ex003/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Imprimir numeros pares de 0 ate 40
+        for(int i = 0; i < 40; i++) {
+            if(i % 2 == 0) { //Verifica se i e par
+                System.out.println(i);
+            }
+        }
+    }
+}
